1ce772738da43229312cc6905467b880d2511717
Bryan Szendel <bryan@bigarmor.com>
2021-08-31 11:52:18 -0400

    frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 function App() {
   const useStyles = makeStyles((theme) => ({
@@ -33,49 +41,145 @@ function App() {
       backgroundColor: "#AF7179",
       "&:hover": { backgroundColor: "#9c575b" },
     },
+    headerContent: {},
   }));
 
-  function FormRow(props) {
-    const { hour } = props;
-    return (
-      <React.Fragment>
-        <Grid item xs={3} spacing={1}>
-          <Button variant="contained" className={classes.paperAvailable}>
-            {hour}:00
-          </Button>
-        </Grid>
-        <Grid item xs={3} spacing={1}>
-          <Button variant="contained" className={classes.paperAvailable}>
-            {hour}:15
-          </Button>
-        </Grid>
-        <Grid item xs={3} spacing={1}>
-          <Button variant="contained" className={classes.paperBooked} disabled>
-            Bryan and Leslie Szendel
-          </Button>
-        </Grid>
-        <Grid item xs={3} spacing={1}>
-          <Button variant="contained" className={classes.paperAvailable}>
-            {hour}:45
-          </Button>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+  const getHour = (hour) => {
+    return hour === 0 ? "12" : String(hour);
+  };
+
+  const getMinutes = (minutes) => {
+    return minutes === 0 ? "00" : String(minutes);
+  };
 
   const classes = useStyles();
 
+  const [slots, setSlots] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [clickedId, setClickedId] = useState(null);
+  const [personName, setPersonName] = useState("");
+
+  useEffect(() => {
+    getSlots();
+  }, []);
+
+  const getSlots = () => {
+    axios
+      .get("https://prayfordawn.herokuapp.com/api/slots/")
+      .then((res) => {
+        setSlots(res.data.sort((a, b) => a.id - b.id));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const handleOpen = (id) => {
+    setOpen(true);
+    setClickedId(id);
+    console.log(id);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setClickedId(null);
+    setPersonName("");
+  };
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    axios
+      .put(`https://prayfordawn.herokuapp.com/api/slots/${clickedId}`, {
+        person_name: personName,
+      })
+      .then((res) => {
+        console.log(res.data);
+        handleClose();
+        getSlots();
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div class="App">
-      <h2 class="header">Pray for Dawn</h2>
+    <div className="App">
+      <div className="headerContainer">
+        <h2 className="headerTitle">Pray for Dawn</h2>
+        <p className="headerContent">
+          Please select a 15 minute timeslot below. The prayer chain for Dawn
+          Whitehurst begins at Midnight on September 1. When you select a slot,
+          please input your name and Save so that Wade can be encouraged by
+          everyone praying for Dawn during these delicate 24 hours.
+        </p>
+        <p className="headerContent">
+          (Please direct any technical issues to Bryan Szendel{" "}
+          <a href="sms:19048878616">(904) 887-8616</a> and not Wade)
+        </p>
+      </div>
       <div className="classes.root">
         <Grid container spacing={1}>
-          <FormRow hour={12} />
-          <FormRow hour={1} />
-          <FormRow hour={2} />
-          <FormRow hour={3} />
+          {slots.map((slot, index) => {
+            return slot.person_name === null ? (
+              <Grid item xs={3} spacing={1} key={slot.id}>
+                <Button
+                  id={slot.id}
+                  onClick={() => handleOpen(slot.id)}
+                  variant="contained"
+                  className={classes.paperAvailable}
+                >
+                  {getHour(slot.hour) + ":" + getMinutes(slot.minutes)}
+                </Button>
+              </Grid>
+            ) : (
+              <Grid item xs={3} spacing={1} key={slot.id}>
+                <Button
+                  id={slot.id}
+                  // onClick={() => handleOpen(slot.id)}
+                  disabled
+                  variant="contained"
+                  className={classes.paperBooked}
+                >
+                  {slot.person_name}
+                </Button>
+              </Grid>
+            );
+          })}
         </Grid>
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">
+          Sign Up For This Time Slot
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter your name to sign up for this prayer time slot.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="personName"
+            label="Person Name"
+            type="text"
+            value={personName}
+            onChange={handleChange}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
